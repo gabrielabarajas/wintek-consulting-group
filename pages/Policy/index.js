267059dab39f576d9d37e72e0a7c8b583a7e27f0
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2024-01-06 12:30:43 +0100

    Changes

--- pages/Policy/index.js
+++ pages/Policy/index.js
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react';
-import PageTitle from '../../components/PageTitle/PageTitle'
+import PageTitle from './page-title/PageTitle'
 import Scrollbar from '../../components/scrollbar/scrollbar'
 import PrivatePolicy from '../../components/PrivatePolicy/PrivatePolicy';
 
